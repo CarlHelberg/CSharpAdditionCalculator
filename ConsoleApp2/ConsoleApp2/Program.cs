d6b03f9a595cd53189541c77814ec88bf6dc81c9
Carl Helberg <carlhelberg.se@gmail.com>
2019-05-21 06:32:19 +0200

    Add files via upload
    Initial commit

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace basicCalculator
+{
+    public class App
+    {
+        public static void Main(string[] args)
+        {
+            bool firstValuecheck = false;
+            bool secondValuecheck = false;
+            double numValue1 = 0;
+            double numValue2 = 0;
+            while (firstValuecheck == false)
+            {
+                Console.WriteLine("Please input the first value");
+                string value1 = Console.ReadLine();
+
+                try
+                {
+                    numValue1 = Convert.ToDouble(value1);
+                    firstValuecheck = true;
+                }
+                catch
+                {
+                    Console.WriteLine("The first Value was not valid");
+                    firstValuecheck = false;
+                }
+            }
+
+            while (secondValuecheck == false)
+            {
+                Console.WriteLine("Please input the second value");
+                string value2 = Console.ReadLine();
+
+                try
+                {
+                    numValue2 = Convert.ToDouble(value2);
+                    secondValuecheck = true;
+                }
+
+                catch
+                {
+                    Console.WriteLine("The second Value was not valid");
+                    secondValuecheck = false;
+                }
+            }
+
+           
+
+            if (firstValuecheck  == true && secondValuecheck == true)
+            {
+                Console.WriteLine("Value 1 + Value 2 =");
+                Console.WriteLine( numValue1 + numValue2);
+            }
+            else
+            {
+                Console.WriteLine("Did not run successful");
+            }
+            Console.Read();
+
+        }
+    }
+}
